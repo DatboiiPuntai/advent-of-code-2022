188cfa0986a38bb707be26498c6d7c59b0089bf6
PantaiSuyasri <pantaisuyasri@gmail.com>
2022-12-02 16:13:16 +0700

    yeet

---
+++ Day2/aoc202202.py
@@ -0,0 +1,13 @@
+def main():
+    with open('input.txt', 'r') as f:
+        lines = f.read()
+
+    games = lines.split('\n')
+    games = [game.split(' ') for game in games]
+    
+    for game in games:
+        if
+
+
+if __name__ == '__main__':
+    main()
