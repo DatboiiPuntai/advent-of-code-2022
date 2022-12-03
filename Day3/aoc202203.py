adfe292c2825256a7111daa7690a2089fbe0f4be
Pantai <pantaisuyasri@gmail.com>
2022-12-03 14:51:24 +0700

    bruh

---
+++ Day3/aoc202203.py
@@ -0,0 +1,13 @@
+def main():
+    with open('input.txt', 'r') as f:
+        lines = f.read()
+
+    rucksacks = lines.split('\n')
+    print(rucksacks)
+    
+
+
+
+
+if __name__ == '__main__':
+    main()
