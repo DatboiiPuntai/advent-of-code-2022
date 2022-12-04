09a78deefeda6bcc00b36af1273d62e4bd5c66dc
Pantai <pantaisuyasri@gmail.com>
2022-12-04 19:49:54 +0700

    day4

---
+++ Day4/aoc202204.py
@@ -0,0 +1,31 @@
+def main():
+    with open(r'D:\Users\Pantai Suyasri\Documents\Programming\advent-of-code-2022\Day4\input.txt', 'r') as f:
+        lines = f.read()
+
+    assignments = [line.split(',') for line in lines.split('\n')]
+
+    for i in range(len(assignments)):
+        assignments[i][0] = list(map(int, assignments[i][0].split('-')))
+        assignments[i][1] = list(map(int, assignments[i][1].split('-')))
+
+    # part 1
+    count = 0
+    for pair in assignments:
+        set1 = set(range(pair[0][0], pair[0][1] + 1))
+        set2 = set(range(pair[1][0], pair[1][1] + 1))
+        if set1.issubset(set2) or set2.issubset(set1):
+            count += 1
+    print(count)
+
+    # part 2
+    count = 0
+    for pair in assignments:
+        set1 = set(range(pair[0][0], pair[0][1] + 1))
+        set2 = set(range(pair[1][0], pair[1][1] + 1))
+        if set1.intersection(set2):
+            count += 1
+    print(count)
+
+
+if __name__ == '__main__':
+    main()
