89906afb6b47e4434d37502f08fa0a18c214f4c4
PantaiSuyasri <pantaisuyasri@gmail.com>
2022-12-06 16:41:31 +0700

    day 6

---
+++ Day6/aoc202206.py
@@ -0,0 +1,22 @@
+def main():
+    with open(r'Day6\input.txt', 'r') as f:
+        line = f.read()
+    
+    chars = []
+    for i in range(len(line)):
+        c = line[i]
+        
+        if c in chars:
+            while c in chars:
+                chars.pop(0)
+            chars.append(c)
+        else:
+            chars.append(c)
+        
+        if len(chars) == 14:
+            print(i+1)
+            break
+    
+
+if __name__ == '__main__':
+    main()
