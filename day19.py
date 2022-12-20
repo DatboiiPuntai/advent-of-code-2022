aa0e401a90822d95b524abfae57312bb6c8c9429
Pantai <pantaisuyasri@gmail.com>
2022-12-20 14:25:03 +0700

    ogey

--- day19.py
+++ day19.py
@@ -3,7 +3,7 @@ import re
 import math
 
 DAY = 19
-TEST = True
+TEST = False
 
 input_str = read_input(day=DAY, test=TEST)
 
@@ -11,16 +11,17 @@ input_str = read_input(day=DAY, test=TEST)
 def dfs(bp, maxspend, cache, time, bots, amt):
     if time == 0:
         return amt[3]
+    
     key = tuple([time, *bots, *amt])
     if key in cache:
         return cache[key]
-
+    
     maxval = amt[3] + bots[3] * time
     
     for btype, recipe in enumerate(bp):
         if btype != 3 and bots[btype] >= maxspend[btype]:
             continue
-
+    
         wait = 0
         for ramt, rtype in recipe:
             if bots[rtype] == 0:
@@ -31,18 +32,16 @@ def dfs(bp, maxspend, cache, time, bots, amt):
             if remtime <= 0:
                 continue
             bots_ = bots[:]
-            amt_ = [x+y * (wait+1) for x, y in zip(amt, bots)]
+            amt_ = [x + y * (wait + 1) for x, y in zip(amt, bots)]
             for ramt, rtype in recipe:
                 amt_[rtype] -= ramt
             bots_[btype] += 1
             for i in range(3):
                 amt_[i] = min(amt_[i], maxspend[i] * remtime)
-
-            maxval = max(maxval, dfs(bp, maxspend, cache, time, bots_, amt_))
-
+            maxval = max(maxval, dfs(bp, maxspend, cache, remtime, bots_, amt_))
+    
     cache[key] = maxval
     return maxval
-
 
 total = 0
 
@@ -57,7 +56,25 @@ for i, line in enumerate(input_str.split('\n')):
             recipe.append((x, y))
             maxspend[y] = max(maxspend[y], x)
         bp.append(recipe)
+
     v = dfs(bp, maxspend, {}, 24, [1, 0, 0, 0], [0, 0, 0, 0])
     total += (i+1) * v
+print(total)
+
+total = 1 
+for i, line in enumerate(input_str.split('\n')[:3]):
+    bp = []
+    maxspend = [0, 0, 0]
+    for section in line.split(': ')[1].split('. '):
+        recipe = []
+        for x, y in re.findall(r'(\d+) (\w+)', section):
+            x = int(x)
+            y = ['ore', 'clay', 'obsidian'].index(y)
+            recipe.append((x, y))
+            maxspend[y] = max(maxspend[y], x)
+        bp.append(recipe)
+
+    v = dfs(bp, maxspend, {}, 32, [1, 0, 0, 0], [0, 0, 0, 0])
+    total *= v
 
 print(total)
