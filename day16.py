b48909672bcaaa09482976af11fceb14ec5081a2
Pantai <pantaisuyasri@gmail.com>
2022-12-21 10:24:10 +0700

    thanks tutorial very cool

--- day16.py
+++ day16.py
@@ -1,74 +1,81 @@
 from utils import read_input
-from collections import defaultdict
-from math import inf
-from operator import itemgetter
-from itertools import combinations, product
-
+from collections import deque
 DAY = 16
-TEST = True
-
-def floyd_warshall(g):
-	distance = defaultdict(lambda: defaultdict(lambda: inf))
-
-	for a, bs in g.items():
-		distance[a][a] = 0
-
-		for b in bs:
-			distance[a][b] = 1
-			distance[b][b] = 0
-
-	for a, b, c in product(g, g, g):
-		bc, ba, ac = distance[b][c], distance[b][a], distance[a][c]
-
-		if ba + ac < bc:
-			distance[b][c] = ba + ac
-
-	return distance
-
-def score(rates, valves):
-	s = 0
-	for v, t in valves.items():
-		s += rates[v] * t
-	return s
-    
-def choices(distance, rates, valves, time=30, cur='AA', chosen={}):
-	for nxt in valves:
-		new_time = time - distance[cur][nxt] - 1
-		if new_time < 2:
-			continue
-
-		new_chosen = chosen | {nxt: new_time}
-		yield from choices(distance, rates, valves - {nxt}, new_time, nxt, new_chosen)
-
-	yield chosen
-
-def parse(input_str: str):
-    data = []
-    for line in input_str.split('\n'):
-        name = line.split()[1]
-        rate = int(line.split()[4][5:-1])
-        tunnels = line.replace('valves', 'valve').split(
-            'valve ')[1].split(', ')
-        data.append((name, rate, tunnels))
-
-    return data
+TEST = False
 
 
-def part1(data):
+input_str = read_input(day=DAY, test=TEST)
 
-    pass
+valves = {}
+tunnels = {}
 
-
-def part2(data):
-    pass
-
-
-def main():
-    input_str = read_input(day=DAY, test=TEST)
-    data = parse(input_str)
-    print(f"Part 1: {part1(data)}")
-    print(f"Part 2: {part2(data)}")
+for line in input_str.splitlines():
+    valve = line.split()[1]
+    flow = int(line.split()[4].split('=')[1][:-1])
+    targets = line.replace("valves", "valve").split('valve ')[1].split(', ')
+    valves[valve] = flow
+    tunnels[valve] = targets
 
 
-if __name__ == '__main__':
-    main()
+dists = {}
+nonempty = []
+
+for valve in valves:
+    if valve != 'AA' and not valves[valve]:
+        continue
+
+    if valve != 'AA':
+        nonempty.append(valve)
+
+    dists[valve] = {valve: 0, 'AA': 0}
+    visited = {valve}
+
+    queue = deque([(0, valve)])
+    while queue:
+        distance, position = queue.popleft()
+        for neighbor in tunnels[position]:
+            if neighbor in visited:
+                continue
+            visited.add(neighbor)
+            if valves[neighbor]:
+                dists[valve][neighbor] = distance + 1
+            queue.append((distance + 1, neighbor))
+
+    del dists[valve][valve]
+    if valve != 'AA':
+        del dists[valve]['AA']
+
+indices = {}
+for index, element in enumerate(nonempty):
+    indices[element] = index
+
+cache = {}
+
+
+def dfs(time, valve, bitmask):
+    if (time, valve, bitmask) in cache:
+        return cache[(time, valve, bitmask)]
+
+    maxval = 0
+    for neighbor in dists[valve]:
+        bit = 1 << indices[neighbor]
+        if bitmask & bit:
+            continue
+        remtime = time - dists[valve][neighbor] - 1
+        if remtime <= 0:
+            continue
+        maxval = max(maxval, dfs(remtime, neighbor, bitmask |
+                     bit) + valves[neighbor] * remtime)
+    cache[(time, valve, bitmask)] = maxval
+    return maxval
+	
+# part 1
+print(dfs(30, 'AA', 0))
+
+# part 2
+b = (1 << len(nonempty)) - 1
+m = 0
+for i in range((b+1)//2):
+    m = max(m, dfs(26, "AA", i) + dfs(26, 'AA', b ^ i))
+
+print(m)
